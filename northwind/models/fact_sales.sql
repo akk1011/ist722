fd8d4f2db060efac8512e3a39ee67c2153a8fed8
Keerthi Krishna <kaiyappa@g.syr.edu>
2024-10-24 03:35:13 +0000

    aa

---
+++ northwind/models/fact_sales.sql
@@ -0,0 +1,57 @@
+with stg_orders as 
+(
+    select
+        orderid,  
+        {{ dbt_utils.generate_surrogate_key(['employeeid']) }} as employeekey, 
+        {{ dbt_utils.generate_surrogate_key(['customerid']) }} as customerkey, 
+        replace(to_date(orderdate)::varchar,'-','')::int as orderdatekey,
+        replace(to_date(shippeddate)::varchar,'-','')::int as shippeddatekey,
+        replace(to_date(requireddate)::varchar,'-','')::int as requireddatekey,
+        shipname,
+        shipaddress,
+        shipcity,
+        shipregion,
+        shippostalcode,
+        shipcountry,
+        freight,
+        shipvia
+    from {{ source('northwind', 'Orders') }}
+),
+stg_order_details as
+(
+    select 
+        orderid,
+        sum(quantity) as quantityonorder, 
+        sum(quantity * unitprice * (1 - discount)) as totalorderamount,
+        sum(quantity * unitprice) as extendedpriceamount,
+        sum(quantity * unitprice * discount) as discountamount
+    from {{ source('northwind', 'Order_Details') }}
+    group by orderid
+),
+stg_shippers as (
+    select 
+        shipperid, 
+        companyname as shippercompanyname
+    from {{ source('northwind', 'Shippers') }}
+)
+
+select  
+    o.orderid,
+    o.employeekey,
+    o.customerkey,
+    o.orderdatekey,
+    o.shippeddatekey,
+    o.requireddatekey,
+    od.quantityonorder, 
+    od.totalorderamount,
+    od.extendedpriceamount,
+    od.discountamount,
+    od.totalorderamount - od.discountamount as soldamount,
+    s.shippercompanyname,
+    o.shippeddatekey - o.orderdatekey as daysfromordertoshipped,
+    o.requireddatekey - o.orderdatekey as daysfromordertorequired,
+    o.shippeddatekey - o.requireddatekey as shippedtorequireddelta,
+    case when o.shippeddatekey - o.requireddatekey <= 0 then 'Y' else 'N' end as shippedontime
+from stg_orders o
+    join stg_order_details od on o.orderid = od.orderid
+    left join stg_shippers s on o.shipvia = s.shipperid
